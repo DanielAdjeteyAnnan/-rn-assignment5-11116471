322458be285789ab0a934881fe4720de925cb533
Daniel Adjetey Annan <daannan001@st.ug.edu.gh>
2024-06-25 17:31:47 +0000

    Home Screen almost done. Bottom TapBar also completed

--- App-Project-V/App.js
+++ App-Project-V/App.js
@@ -1,20 +1,60 @@
+import React from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View,Image } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import {HomeScreen} from './components/homeScreen';
+import {SettingsScreen} from './components/settings';
+import {MyCardScreen} from './components/cardsScreen';
+import {StatisticsScreen} from './components/statistics'
+
+const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <View style={styles.container}>
+
+        <Tab.Navigator>
+        <Tab.Screen
+          name="Home" component={HomeScreen} options={{ headerShown: false,tabBarLabel: 'Home',
+            tabBarIcon: ({color, size}) => (
+              <Image source={require('./assets/images/home.png')} style={{ width: size, height: size, tintColor: color }}/>),
+          }}
+        />
+
+        <Tab.Screen
+          name="Card"component={MyCardScreen} options={{ headerShown: false, tabBarLabel: 'My Card',
+            tabBarIcon: ({color, size}) => (
+              <Image source={require('./assets/images/myCards.png')} style={{ width: size, height: size, tintColor: color }} />), 
+           }} 
+           />
+          
+
+        <Tab.Screen
+          name="statistics"component={StatisticsScreen} options={{ headerShown: false, tabBarLabel: 'Statistics',
+            tabBarIcon: ({color, size}) => (
+              <Image source={require('./assets/images/statictics.png')}style={{ width: size, height: size, tintColor: color }} />), 
+           }} 
+           />
+
+        <Tab.Screen
+          name="Settings"component={SettingsScreen} options={{ headerShown: false, tabBarLabel: 'Settings',
+            tabBarIcon: ({color, size}) => (
+              <Image source={require('./assets/images/settings.png')} style={{ width: size, height: size, tintColor: color }} />), 
+           }}
+            />
+
+        </Tab.Navigator>
+        <StatusBar style="auto" />
+      </View>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+
   },
 });
---
+++ App-Project-V/components/homeScreen.js
@@ -0,0 +1,265 @@
+
+import React from 'react';
+import {StyleSheet,Image, Text, View, ScrollView, } from 'react-native';
+
+export const HomeScreen = () =>{
+    return(
+      <ScrollView contentContainerStyle={styles.scrollViewContent}>
+        <View style={styles.home}>
+            <View style={styles.proCon}>
+                <Image source={require('../assets/images/profile.png')} style={styles.profile}></Image>
+                <View style={styles.proText}>
+                    <Text  style={styles.wel}>Welcome back,</Text>
+                    <Text style={styles.dAA}>Daniel Annan</Text> 
+                </View>
+                <View style={styles.searchCon}>
+                    <Image source={require('../assets/images/search.png')} style={styles.search}></Image>
+                </View>
+            </View>
+          
+             <Image source={require('../assets/images/Card.png')} style={styles.creCard}></Image>
+
+             <View style={styles.trans}>
+             <View style={styles.tranIconWrapper}>
+                    <View style={styles.tranIcon}>
+                        <Image source={require('../assets/images/send.png')} style={styles.icons} />
+                    </View>
+                    <Text style={styles.iconText}>Sent</Text>
+                </View>
+
+                <View style={styles.tranIconWrapper}>
+                    <View style={styles.tranIcon}>
+                        <Image source={require('../assets/images/receive.png')} style={styles.icons} />
+                    </View>
+                    <Text style={styles.iconText}>Receive</Text>
+                </View>
+
+                <View style={styles.tranIconWrapper}>
+                    <View style={styles.tranIcon}>
+                        <Image source={require('../assets/images/loan.png')} style={styles.icons} />
+                    </View>
+                    <Text style={styles.iconText}>Loan</Text>
+                </View>
+
+                <View style={styles.tranIconWrapper}>
+                    <View style={styles.tranIcon}>
+                        <Image source={require('../assets/images/topUp.png')} style={styles.icons} />
+                    </View>
+                    <Text style={styles.iconText}>TopUp</Text>
+                </View>
+                
+            
+            </View>
+            <View style={styles.tranSee}>
+              <Text style={styles.tranSaction}>Transaction</Text>
+              <Text style={styles.sellAll}>Sell All</Text>
+            </View>
+
+            <View style={styles.transactions}>
+            <View style={styles.tranWrap}>
+                    <View style={styles.tranIcon}>
+                        <Image source={require('../assets/images/apple.png')} style={styles.icons} />
+                    </View>
+                    <View style={styles.tranText}> 
+                            <Text style={styles.mainText}>Apple Store</Text> 
+                            <Text style={styles.subText}>Entertainment</Text>
+                     </View> 
+                     <View style={styles.selary}>
+                        <Text style={styles.amount}> -$5,99</Text>
+                       </View>   
+             </View>
+
+             <View style={styles.tranWrap}>
+                    <View style={styles.tranIcon}>
+                        <Image source={require('../assets/images/apple.png')} style={styles.icons} />
+                    </View>
+                    <View style={styles.tranText}> 
+                            <Text style={styles.mainText}>Apple Store</Text> 
+                            <Text style={styles.subText}>Entertainment</Text>
+                     </View> 
+                     <View style={styles.selary}>
+                        <Text style={styles.amount}> -$5,99</Text>
+                       </View>   
+             </View>
+
+             <View style={styles.tranWrap}>
+                    <View style={styles.tranIcon}>
+                        <Image source={require('../assets/images/apple.png')} style={styles.icons} />
+                    </View>
+                    <View style={styles.tranText}> 
+                            <Text style={styles.mainText}>Apple Store</Text> 
+                            <Text style={styles.subText}>Entertainment</Text>
+                     </View> 
+                     <View style={styles.selary}>
+                        <Text style={styles.amount}> -$5,99</Text>
+                       </View>   
+             </View>
+
+             <View style={styles.tranWrap}>
+                    <View style={styles.tranIcon}>
+                        <Image source={require('../assets/images/apple.png')} style={styles.icons} />
+                    </View>
+                    <View style={styles.tranText}> 
+                            <Text style={styles.mainText}>Apple Store</Text> 
+                            <Text style={styles.subText}>Entertainment</Text>
+                     </View> 
+                     <View style={styles.selary}>
+                        <Text style={styles.amount}> -$5,99</Text>
+                       </View>   
+             </View>
+
+            </View>
+
+         </View>
+         </ScrollView>
+
+    )
+};
+
+const styles =StyleSheet.create({
+  scrollViewContent: {
+    flexGrow: 1,
+    paddingBottom: 50,
+  },
+    home:{
+      flex: 1,
+      paddingTop: 10,
+      paddingHorizontal: 20,
+        
+    },
+    profile:{
+        width: 50,
+        height: 50,
+        borderRadius: 70,
+        right:20,
+        top:40
+
+    },
+    proText:{
+      gap:5,
+      bottom:13,
+      left:50
+
+    },
+    proCon:{
+      left:20
+
+    },
+    dAA:{
+      fontWeight:'bold',
+      fontSize:24
+    },
+    wel:{
+      color:'#95969D',
+      fontSize:15
+    },
+    searchCon:{
+        width: 50,
+        height: 50,
+        borderRadius: 25,
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: 'rgba(0, 0, 0, 0.1)',
+        left:270,
+        bottom:65
+
+    },
+    search:{
+     width: 30, 
+    height: 30,
+       
+    },
+    creCard:{
+      width:350,
+      height:200,
+    borderRadius:20,
+    bottom:20
+    },
+    trans:{
+      flexDirection:'row',
+        justifyContent: 'space-around',
+        top:15
+    },
+    tranIconWrapper: {
+      alignItems: 'center',
+  },
+  tranIcon:{
+    width: 60,
+    height: 60,
+    borderRadius: 30,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'rgba(0, 0, 0, 0.1)',
+    
+  },
+  iconText: {
+    marginTop: 10,
+    textAlign: 'center',
+    fontSize: 15,
+    color: '#000'
+},
+tranSee:{
+  flexDirection:'row',
+  top:40
+},
+tranSaction:{
+  fontWeight:'bold',
+  fontSize:24
+},
+sellAll:{
+  marginLeft:158,
+  top:5,
+  fontSize:20,
+  color: 'blue'
+},
+transactions:{
+  top:50,
+  right:15,
+  
+},
+tranText:{
+  gap:15,
+  marginTop:10,
+},
+tranWrap:{
+  flexDirection:'row',
+    borderRadius:20,
+    gap:10,
+    padding:10,
+
+},
+mainText:{
+  fontSize: 18,
+  fontWeight: 'bold',
+  bottom: 5,
+  color: '#0D0D26',
+  left:12
+
+},
+subText:{
+  color: '#95969D',
+    bottom: 13,
+    left: 12,
+   fontSize: 13,
+
+
+},
+selary:{
+  marginLeft:90,
+  marginTop:20,
+  gap:15,
+
+},
+amount:{
+  color: '#0D0D26',
+        fontSize:18,
+        left:20,
+        fontWeight:'bold'
+
+},
+     
+
+})
+
+
+ 
